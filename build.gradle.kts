528c17c612b75bbf74f14f765a0a759aa1f5ea67
Liam Highcock <liamhigh78@gmail.com>
2025-09-08 03:37:25 +0200

    Add Android root build files (.gitignore, settings, build)

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    // AGP (adjust to your app’s version later if needed)
+    id("com.android.application") version "8.4.2" apply false
+    kotlin("android") version "1.9.24" apply false
+}
